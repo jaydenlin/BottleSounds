abd00eaef2c7b87c3213961e9673e01c85a93bbd
KuanWei Lee <pc042672@gmail.com>
2014-05-25 14:59:07 +0800

    Combines CirCleIcon and UserData

--- src/com/pheelicks/visualizer/HistoryView.java
+++ src/com/pheelicks/visualizer/HistoryView.java
@@ -1,41 +1,28 @@
 package com.pheelicks.visualizer;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
 import android.app.AlertDialog.Builder;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Paint.Cap;
-import android.graphics.PorterDuff.Mode;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import com.wearapp.HistoryActivity;
-import com.wearapp.ListenActivity;
 import com.wearapp.HistoryActivity.UserData;
 import com.wearapp.R;
 
@@ -59,7 +46,7 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 	private boolean flag = true;
 	private Canvas canvas = null;
 	private SurfaceHolder holder;
-	private ArrayList<CircleIcon> iconList; // ICON 類別型態的物件陣列
+	 HashMap<Long, UserData> userMap;	
 	private Thread db_thread;
 	float mCenterX; 
 	float mCenterY; 
@@ -69,12 +56,10 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 	private HistoryActivity mActivity;
 	private HashMap<Long,UserData> mUserIdMap ;
 	private Builder mAlertDialogBuilder; 
-    private ProgressDialog progressDialog;
  
   
 	Paint slinePaint;
 	Paint textPaint;
-	Paint iconPaint;
 	
 
 	public HistoryView(Context context, AttributeSet attrs, int defStyle) {
@@ -90,17 +75,15 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 		if(D_METHOD){
 			Log.w(TAG, "On Create HistoryView");
 		}
+		this.userMap = new HashMap<Long, HistoryActivity.UserData>();
 		mActivity = activity;
 		mContext = context;
 		res = getResources();
 		setWillNotDraw(false);
 		getHolder().addCallback(this);
 		holder = getHolder();
-			
-		iconList = new ArrayList<CircleIcon>();
 		slinePaint = new Paint();
 		textPaint = new Paint();
-		iconPaint = new Paint();
 		
 		InitialSetting();
 		
@@ -112,13 +95,7 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 		if(D_METHOD){
 			Log.w(TAG, "In InitialSet");
 		}
-		iconList.clear(); 
-
-		progressDialog = new ProgressDialog(mActivity);
-		progressDialog.setMessage("Loading message. Please wait...");
-		progressDialog.setIndeterminate(false);
-		progressDialog.setCancelable(false);
-		progressDialog.show();
+	
 	/* draw vertical line in the background */
 		slinePaint.setStrokeWidth(3f);
 		slinePaint.setColor(Color.WHITE);
@@ -132,19 +109,14 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 		textPaint.setAlpha(80);
 		textPaint.setStyle(Paint.Style.STROKE);
 		
-		iconPaint.setStrokeWidth(4f);
-		iconPaint.setColor(Color.WHITE);
-		//iconPaint.setAlpha(200);
-		iconPaint.setStyle(Paint.Style.STROKE);
-		iconPaint.setStrokeCap(Cap.ROUND);
-		iconPaint.setAntiAlias(true);
+
 	}
 	
 	@Override
 	public void run() {
 		while (flag) {
 			if(D_SHOW_TAG){
-				Log.w(TAG, "In HistoryView run + iconList size = "+iconList.size());
+				Log.w(TAG, "In HistoryView run ");
 			}			
 			lockCanvasAndDrawHistoryView();
 			sleepThread();
@@ -226,10 +198,18 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 
 	}
 	
+	public HashMap<Long, HistoryActivity.UserData> getUserMap(){
+
+		
+		return this.userMap;
+	}
 
 	public void drawIcons() {
-		for (CircleIcon icon : iconList) {
-			icon.PostUnit(canvas);
+		Log.w(TAG, "In drawIcons()");
+		for (UserData user : userMap.values()){
+			if(user.getUserPic()!= null){
+				user.PostUnit(canvas);
+			}
 		}
 		refetchUserMap();
 	}	
@@ -239,14 +219,13 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 		if(D_METHOD){
 			Log.w(TAG, "in refresh");
 		}
-		if(!refetchingUserMap){
-			return;
-		}
-		
+//		if(!refetchingUserMap){
+//			return;
+//		}
+//		
 		if(mUserIdMap != null){
 			traverseMap(mUserIdMap);
 			refetchingUserMap = false; 
-            progressDialog.dismiss();
 		}		
 	}
 
@@ -260,7 +239,11 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 	    	UserData userdata = (UserData)pairs.getValue();
 	    	if(userdata.isOwner()){
 	    		//TODO not a good way, need to modify
-	    		iconList.add(new CircleIcon(userdata.getUserPic() , this.mCenterX, this.mCenterY, userdata));
+	    		userdata.setsCircleIcon(  this.mCenterX, this.mCenterY);
+	    		continue;
+	    	}
+	    	if(userdata.getUserPic() == null ){
+	    		Log.w(TAG, userdata.getUID()+" still not get profile picture");
 	    		continue;
 	    	}
 	    	
@@ -269,14 +252,15 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 	    	if(D_SHOW_TAG){
 	    		Log.w(TAG, "random x, y "+x+ " "+ y);
 	    	}
-	    	iconList.add(new CircleIcon(userdata.getUserPic() , x, y, userdata));
+	    	userdata.setsCircleIcon(x, y);	    	
 	    	
 	    }
+	   
 	}
 	
 	private void sleepThread(){
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -284,7 +268,7 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 	
 	@Override
 	public void onDraw(Canvas canvas) {
-		Log.w(TAG, "in onDraw()");
+		//Log.w(TAG, "in onDraw()");
 
 	}
 	
@@ -293,7 +277,7 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             int x = (int)event.getX();
             int y = (int)event.getY();
-            for (CircleIcon a: iconList) {
+            for (UserData a: userMap.values()) {
                 a.IsTouch(x, y);
             }
         }
@@ -331,150 +315,5 @@ public class HistoryView extends SurfaceView implements SurfaceHolder.Callback,
 		this.mUserIdMap = mUserIdMap;
 	}
 	
-
-
-	public class CircleIcon implements Runnable {
-		private Bitmap unit_bmp;
-		Rect unit_rect = new Rect(); 
-	    private AlertDialog mAlertDialog;
-	    private float circleX ;
-	    private float circleY ;
-	    private UserData userData ;
-	    private int index;
-		int unit_Width ;
-		int unit_Height;
-
-	    
-	    private void setUserData(UserData usd){
-	    	
-	    	userData = usd;
-	    }
- 
-		public void setX(float _x) {
-			circleX = _x;
-		}
-
-		public void setY(float _y) {
-			circleY = _y;
-		}
-		
-		public void setindex(int i){
-			
-			index =i;
-		}
-		public int getIndex(){
-			return index; 
-		}
-	    
-		public CircleIcon(Bitmap icon_pic , float _x, float _y, UserData userdata) {
-			if(D_METHOD){
-				Log.w(TAG,"In CircleIcon");
-			}
-			this.unit_bmp = icon_pic;
-			unit_Width = unit_bmp.getWidth();
-			unit_Height = unit_bmp.getHeight();
-			circleX= _x;
-			circleY = _y;
-			userData = userdata;
-			// CircleIcon 類別實作了 Runnable 介面
-			// 仍需以 Thread 類別來建立執行緒，如下 :
-			new Thread(this).start();
-
-			// 將 AndroidUnit 類別本身(this)當做參數並實體化為執行緒
-			// 啟動 .start() 方法會自動執行 run() 函式內的程式內容
-			// 當 AndroidUnit 此類別 被實體化時，便會同時啟動此類別的執行緒
-			// 例如 : AndroidUnit au = new AndroidUnit(bmp);
-
-		}
-
-		@Override 
-		public void run() {}
-
-		// ==== 將圖 PO 到 canvas(畫布)上 ====
-		protected void PostUnit(Canvas canvas) {
-			// 在 canvas 上繪出物件本體
-			float X =    circleX- unit_Width/2;
-			float Y = circleY - unit_Height/2;
-			float raidus = getRadius(unit_Width/2,unit_Height/2);
-			Bitmap newBitmap = getCroppedBitmap(unit_bmp);
-			canvas.drawBitmap(newBitmap,X, Y , null);
-			
-			canvas.drawCircle(circleX, circleY, raidus, iconPaint);
-			unit_rect.set((int)X ,(int)Y , (int)X + unit_Width,(int)Y+ unit_Width) ;
-			
-		}
-		
-		public float getRadius(float width, float height){
-			return (width > height)? height:width;		
-		}
-		
-		 public Bitmap getCroppedBitmap(Bitmap bitmap) {
-		    	
-		    	
-		        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
-		                bitmap.getHeight(), Config.ARGB_8888);
-		        Canvas canvas = new Canvas(output);
-
-		        final int color = 0xff424242;
-		        final Paint paint = new Paint();
-		        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-		        paint.setAntiAlias(true);
-		        canvas.drawARGB(0, 0, 0, 0);
-		        paint.setColor(color);
-		        int diameter  = (bitmap.getWidth() > bitmap.getHeight())? bitmap.getHeight() : bitmap.getWidth();
-		        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
-		        		diameter / 2, paint);
-		        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
-		        canvas.drawBitmap(bitmap, rect, rect, paint);
-
-		        return output;
-		    }
-		
-		//==== 檢查是否被碰觸到 ====
-	    protected void IsTouch(int touch_x, int touch_y) {
-
-	        //將觸碰點的座標 touch_x 與 touch_y 傳入到
-	        //矩形框類別變數 unit_rect 的 contains(x, y) 方法中去判別
-	        //如果觸碰點的座標位於矩形框範圍內則contains(x, y)方法會傳回 true
-	        //否則傳回 false
-        	 if( userData == null || userData.isOwner()){
-        		 
-        		 return;
-        	 }
-
-	        if (unit_rect.contains(touch_x, touch_y)) {
-	        	if(D_SHOW_TAG){
-	        		Log.w("CirecleIcon", "Is touched"+touch_x+" "+touch_y+" "+unit_rect.bottom+" "+unit_rect.left+" "+unit_rect.right+" "+unit_rect.top);
-	        	}
-
-	        	showCustomizedDialog();
-
-	        }
-	    }
-	    
-	    private void showCustomizedDialog(){
-        	final Dialog dialog = new Dialog(mActivity,android.R.style.Theme_Translucent);
-        	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        	dialog.setCancelable(true);
-        	dialog.setContentView(R.layout.dialog);
-        	Button btnComplete = (Button) dialog.findViewById(R.id.button_complete);
-        	TextView text_placename = (TextView) dialog.findViewById(R.id.text_place_name);
-        	text_placename.setText(userData.getPlaceName());
-        	//TextView text_name = (TextView) dialog.findViewById(R.id.text_name);
-        	ImageView image_icon = (ImageView) dialog.findViewById(R.id.image_icon);
-        	image_icon.setImageBitmap(userData.getUserPic());
-        	btnComplete.setOnClickListener(new OnClickListener(){
-
-				@Override
-				public void onClick(View view) {
-					dialog.dismiss();
-				}
-        		
-        	});
-        	dialog.show();
-	    }
-
-	}//CircleIcon()
 
 }//HistoryView
--- src/com/wearapp/HistoryActivity.java
+++ src/com/wearapp/HistoryActivity.java
@@ -22,6 +22,8 @@ import com.wearapp.util.JSONParser;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
@@ -29,27 +31,41 @@ import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.Bitmap.Config;
+import android.graphics.Paint.Cap;
+import android.graphics.PorterDuff.Mode;
 import android.support.v4.util.LongSparseArray;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 public class HistoryActivity extends Activity {
 	
 	private DB mDBHelper;
 	private Cursor mCursor;
 	HistoryView background;
-	
+    private ProgressDialog progressDialog;
+	Paint iconPaint;
+
 	DecimalFormat df2 = new DecimalFormat("00");
 	
-
+	public void doMySearch(String query){}
 	
 	@Override
 	protected void onStop(){
-		super.onStop();
-		
-		
+		super.onStop();	
 	}
 	
 	@Override
@@ -57,16 +73,6 @@ public class HistoryActivity extends Activity {
 	    // Inflate the options menu from XML
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.options_menu, menu);
-	    
-	    /*
-	    // Get the SearchView and set the searchable configuration
-	    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-	    SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-	    // Assumes current activity is the searchable activity
-	    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-	    Log.w("In create search bar", ""+getComponentName());
-	    searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-	     */
 	    return true;
 	}
 	
@@ -83,36 +89,34 @@ public class HistoryActivity extends Activity {
 	      String query = intent.getStringExtra(SearchManager.QUERY);
 	      doMySearch(query);
 	    }
+	    progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage("Loading message. Please wait...");
+		progressDialog.setIndeterminate(false);
+
+		iconPaint = new Paint();
+		iconPaint.setStrokeWidth(4f);
+		iconPaint.setColor(Color.WHITE);
+		//iconPaint.setAlpha(200);
+		iconPaint.setStyle(Paint.Style.STROKE);
+		iconPaint.setStrokeCap(Cap.ROUND);
+		iconPaint.setAntiAlias(true);
 		
-	    
-	    //setAdapter();
- 	    
 	    getItemListFromWeb();
+	    Log.w("HistoryActivity", "After get getItemListFromWeb");
 	    
 	}
 	
-	
-
-	
-
-	public void doMySearch(String query){}
-	
-	
 	//////////////////////////////////////////////
 	//											//
-	//QUERY DATA FROM MYSQL						//
+	//		QUERY DATA FROM Parse				//
 	//											//
 	//////////////////////////////////////////////
 	
   // Creating JSON Parser object
     JSONParser jParser = new JSONParser();
  
-    ArrayList<UserData> userDataList;
     HashMap<Long, UserData> userMap;
  
-    
-    
-    
     // JSON Node names
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_OWNER = "owner";
@@ -121,33 +125,29 @@ public class HistoryActivity extends Activity {
     private static final String TAG_MESSAGE = "message";
     private static final String TAG_PLACENAME = "placeName";
     private static final String TAG_TO_FRIENDS = "toFriends";
+    private static final String TAG_LINK = "ownerPic";
+    
  
     // products JSONArray
     JSONArray products = null;
     private void getItemListFromWeb(){
-        // Hashmap for ListView
-    	userDataList = new ArrayList<UserData>();
-         userMap = new HashMap<Long, UserData>();
- 
+         userMap = background.getmUserIdMap();
         // Loading userIdList in Background Thread
         new LoadAllMessage().execute();
     	
     }
 
- 
-    
-    
     /**
      * Background Async Task to Load all product by making HTTP Request
      * */
     public class LoadAllMessage extends AsyncTask<String, String, String> {
- 
         /**
          * Before starting background thread Show Progress Dialog
          * */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+    		progressDialog.show();
         }
  
         
@@ -168,35 +168,21 @@ public class HistoryActivity extends Activity {
         	    	String userId = toWhomList.get(0);
         	    	userdata = new UserData(Long.parseLong(userId),null,null);
         	    	userdata.setisOwner(true);
-        	    	userDataList.add(userdata);
         	    	userMap.put(userdata.getUID(), userdata);
         	    	new DownloadPictureTask().execute(userdata);
         	    	for(ParseObject parseObject : parseObjectList){
-        	    		//Log.w("ParseObject", "UserData((Long)parseObject.get(TAG_OWNER) = "+ (String)parseObject.get(TAG_OWNER));
+        	    		Log.w("ParseObject", "UserData((Long)parseObject.get(TAG_OWNER) = "+ (String)parseObject.get(TAG_OWNER));
         	    		userdata = new UserData(Long.parseLong((String)parseObject.get(TAG_OWNER)),(String)parseObject.getString(TAG_PLACENAME), null);
         	    		userdata.setLatitude((Double)parseObject.get(TAG_LATITUDE));
         	    		userdata.setLongitude((Double)parseObject.get(TAG_LONGITUDE));
         	    		userdata.setMessage((String)parseObject.get(TAG_MESSAGE));
-        	    		userDataList.add( userdata);
+        	    		userdata.setUserLink((String)parseObject.get(TAG_LINK));
         	    		userMap.put(userdata.getUID(), userdata);
         	    		new DownloadPictureTask().execute(userdata);
         	    		
-        	    		//Log.w("HistoryActivity","In LoadAllMessage doInBackGround" +(String)parseObject.get(TAG_OWNER)+" "+(String) parseObject.getString(TAG_PLACENAME) );
+        	    		Log.w("HistoryActivity","In LoadAllMessage doInBackGround" +(String)parseObject.get(TAG_OWNER)+" "+(String) parseObject.getString(TAG_PLACENAME) );
         	    	}
-        	    	Log.w("HistoryActivity", "userdatalist size = "+userDataList.size());
-        	    	while(true){
-        	    		int cnt = 0;
-        	    		for(UserData user : userDataList){
-        	    			if(user.getUserPic() == null){
-        	    				cnt++;
-        	    			}
-        	    			
-        	    		}
-        	    		if(cnt == 0){
-        	    			background.setmUserIdMap( userMap);
-        	    			break;
-        	    		}
-        	    	}//while()
+        	    	background.setmUserIdMap( userMap);
         	    	
         	    }//afterGetListDone()
 
@@ -219,80 +205,25 @@ public class HistoryActivity extends Activity {
         protected void onPostExecute(String file_url) {
             // dismiss the dialog after getting all products
             // updating UI from Background Thread
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    /**
-                     * Updating parsed JSON data into ListView
-                     * TODO 
-                     * */
-                }
-            });
+//            runOnUiThread(new Runnable() {
+//                public void run() {
+//                    /**
+//                     * Updating parsed JSON data into ListView
+//                     * TODO 
+//                     * */
+//                }
+//            });
+        	
+        	progressDialog.dismiss();
  
         }//onPostExecute()
-        
-        
-        
-
-        
- 
-    }//LoadAllMessages()
+   }//LoadAllMessages()
     
-    private class DownloadPictureTask extends AsyncTask<UserData, Integer, Integer> {
-        
-    	
-    	protected Integer doInBackground(UserData... urls) {
-            int count = urls.length;
-            for (int i = 0; i < count; i++) {
-               
-            	Bitmap bitmap = getFacebookProfilePic(urls[i].getUID());
-            	 userMap.get(urls[i].getUID()).setUserPic(bitmap);
-            	
-                if (isCancelled()) break;
-            }
-            return  userMap.size();
-        }
-
-        protected void onProgressUpdate(Integer... progress) {
-        }
-
-        protected void onPostExecute(Integer result) {
-        	Log.w("Finished Dowload Picture Task",  userMap.size()+"");
-
-        }
     
-        private Bitmap getFacebookProfilePic(Long long1){
-        	Log.w("HistoryActivity", "start getFacebookProfilePic "+ long1);
-
-        	 URL img_value = null;
-        	 Bitmap mIcon = null;
-        	 try {
-    			img_value = new URL("https://graph.facebook.com/"+String.valueOf(long1)+"/picture?type=large");
-    			Log.w("HistoryActivity", img_value.toString());
-    			mIcon = BitmapFactory.decodeStream(img_value.openConnection().getInputStream());
-    			if(mIcon == null ) {
-    				Log.e("HistoryActivity", "get null picutre");
-    			}
-    			
-    			
-    		} catch (MalformedURLException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}catch (IOException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-    		}
-        	Log.w("HistoryActivity", " finish getFacebookProfilePic "+ long1 );
-        	return mIcon;
-        }
-        
-        
-        
-    
-    }
-   
+       
 
 	
-    public class UserData  {
+    public class UserData implements Runnable {
     	private boolean isOwner = false;
     	private Long UID;
 		private String placeName;
@@ -300,12 +231,25 @@ public class HistoryActivity extends Activity {
 		private double latitude;
 		private String message;
 		
-		private Bitmap userPic;
+		private String link;
+		
+
+		private Bitmap unit_bmp;
+		Rect unit_rect = new Rect(); 
+	    private float circleX ;
+	    private float circleY ;
+	    private int index;
+		int unit_Width ;
+		int unit_Height;
+		
+		
 		
     	public UserData(Long uid , String placename, Bitmap bitmap){
     		UID = uid;
     		placeName = placename;
-    		userPic = bitmap;
+    		unit_bmp = bitmap;
+    		link= "";
+
     	}
     	
     	public boolean isOwner(){
@@ -364,15 +308,195 @@ public class HistoryActivity extends Activity {
 		}
 
 		public Bitmap getUserPic() {
-			return userPic;
+			return unit_bmp;
 		}
 
 		public void setUserPic(Bitmap userPic) {
-			this.userPic = userPic;
+			this.unit_bmp = userPic;
 		}
 
-    }
+		
+		public void setUserLink(String link){
+			this.link = link;
+		}
+		
+		public String getUserLink(){
+			return this.link;
+		}
+    
+ 
+		public void setX(float _x) {
+			circleX = _x;
+		}
+
+		public void setY(float _y) {
+			circleY = _y;
+		}
+		
+		public void setindex(int i){
+			
+			index =i;
+		}
+		public int getIndex(){
+			return index; 
+		}
+	    
+		public void setsCircleIcon(float _x, float _y ) {
+			
+			unit_Width = unit_bmp.getWidth();
+			unit_Height = unit_bmp.getHeight();
+			circleX= _x;
+			circleY = _y;
+			// CircleIcon 類別實作了 Runnable 介面
+			// 仍需以 Thread 類別來建立執行緒，如下 :
+			new Thread(this).start();
+
+			// 將 AndroidUnit 類別本身(this)當做參數並實體化為執行緒
+			// 啟動 .start() 方法會自動執行 run() 函式內的程式內容
+			// 當 AndroidUnit 此類別 被實體化時，便會同時啟動此類別的執行緒
+			// 例如 : AndroidUnit au = new AndroidUnit(bmp);
+
+		}
+
+		@Override 
+		public void run() {}
+
+		// ==== 將圖 PO 到 canvas(畫布)上 ====
+		public void PostUnit(Canvas canvas) {
+			// 在 canvas 上繪出物件本體
+			float X =    circleX- unit_Width/2;
+			float Y = circleY - unit_Height/2;
+			float raidus = getRadius(unit_Width/2,unit_Height/2);
+			Bitmap newBitmap = getCroppedBitmap(unit_bmp);
+			canvas.drawBitmap(newBitmap,X, Y , null);
+			
+			canvas.drawCircle(circleX, circleY, raidus, iconPaint);
+			unit_rect.set((int)X ,(int)Y , (int)X + unit_Width,(int)Y+ unit_Width) ;
+			
+		}
+		
+		public float getRadius(float width, float height){
+			return (width > height)? height:width;		
+		}
+		
+		 public Bitmap getCroppedBitmap(Bitmap bitmap) {
+		    	
+		    	
+		        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+		                bitmap.getHeight(), Config.ARGB_8888);
+		        Canvas canvas = new Canvas(output);
+
+		        final int color = 0xff424242;
+		        final Paint paint = new Paint();
+		        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+		        paint.setAntiAlias(true);
+		        canvas.drawARGB(0, 0, 0, 0);
+		        paint.setColor(color);
+		        int diameter  = (bitmap.getWidth() > bitmap.getHeight())? bitmap.getHeight() : bitmap.getWidth();
+		        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+		        		diameter / 2, paint);
+		        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+		        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+		        return output;
+		    }
+		
+		//==== 檢查是否被碰觸到 ====
+	    public void IsTouch(int touch_x, int touch_y) {
+
+	        //將觸碰點的座標 touch_x 與 touch_y 傳入到
+	        //矩形框類別變數 unit_rect 的 contains(x, y) 方法中去判別
+	        //如果觸碰點的座標位於矩形框範圍內則contains(x, y)方法會傳回 true
+	        //否則傳回 false
+        	 if(isOwner()){		 
+        		 return;
+        	 }
+
+	        if (unit_rect.contains(touch_x, touch_y)) {
+	        		Log.w("CirecleIcon", "Is touched"+touch_x+" "+touch_y+" "+unit_rect.bottom+" "+unit_rect.left+" "+unit_rect.right+" "+unit_rect.top);
+	  	        	showCustomizedDialog();
+
+	        }
+	    }
+	    
+	    private void showCustomizedDialog(){
+        	final Dialog dialog = new Dialog(HistoryActivity.this, android.R.style.Theme_Translucent);
+        	dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        	dialog.setCancelable(true);
+        	dialog.setContentView(R.layout.dialog);
+        	Button btnComplete = (Button) dialog.findViewById(R.id.button_complete);
+        	TextView text_placename = (TextView) dialog.findViewById(R.id.text_place_name);
+        	text_placename.setText(getPlaceName());
+        	//TextView text_name = (TextView) dialog.findViewById(R.id.text_name);
+        	ImageView image_icon = (ImageView) dialog.findViewById(R.id.image_icon);
+        	image_icon.setImageBitmap(getUserPic());
+        	btnComplete.setOnClickListener(new OnClickListener(){
+
+				@Override
+				public void onClick(View view) {
+					dialog.dismiss();
+				}
+        		
+        	});
+        	dialog.show();
+	    }
+
+
+    }//UserData
+    
+
+    private class DownloadPictureTask extends AsyncTask<UserData, Integer, Integer> {
+        	
+    	protected Integer doInBackground(UserData... urls) {
+            int count = urls.length;
+            for (int i = 0; i < count; i++) {
+               
+            	Bitmap bitmap = getFacebookProfilePic(userMap.get(urls[i].getUID()), urls[i].getUID());
+            	 userMap.get(urls[i].getUID()).setUserPic(bitmap);
+            	
+                if (isCancelled()) break;
+            }
+            return  userMap.size();
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(Integer result) {
+        	Log.w("Finished Dowload Picture Task",  userMap.size()+"");
+
+        }
+    
+        private Bitmap getFacebookProfilePic(UserData userdata, Long long1){
+        	Log.w("HistoryActivity", "start getFacebookProfilePic "+ long1);
+
+        	 URL img_value = null;
+        	 Bitmap mIcon = null;
+        	 try {
+        		 
+    			img_value = new URL("https://graph.facebook.com/"+String.valueOf(long1)+"/picture?type=large");
+        		 //img_value = new URL(userdata.getUserLink());
+        		 Log.w("HistoryActivity", img_value.toString());
+    			mIcon = BitmapFactory.decodeStream(img_value.openConnection().getInputStream());
+    			if(mIcon == null ) {
+    				Log.e("HistoryActivity", "get null picutre");
+    			}
+    			
+    			
+    		} catch (MalformedURLException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        	Log.w("HistoryActivity", " finish getFacebookProfilePic "+ long1 );
+        	return mIcon;
+        }
+
+    }//DownloadPictureTask
+
     
     
-    
-}
+}//HistiryActivity
